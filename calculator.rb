68019674cf01c30a75d60334b29c60433130ed03
Alex Mata <alexmata92@ymail.com>
2020-07-27 16:34:41 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,7 @@ first_number = 27
 
 second_number = 4
 
-sum = #{first_number} + #{second_number}
+sum = first_number + second_number
 
-difference = #{first_number} - #{second_number}
+difference = first_number - second_number
 
