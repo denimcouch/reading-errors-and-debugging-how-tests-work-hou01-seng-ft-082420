8c149b690819f516b3a578e9a31f28e52ea1aa5a
Alex Mata <alexmata92@ymail.com>
2020-07-27 16:27:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
 # Add your variables here
+first_number = 27
+second_number = 4
+
+return `#{first_number} + #{second_number}`
+
+return `#{first_number} - #{second_number}`
+
+return `#{first_number} * #{second_number}`
+
+divide
