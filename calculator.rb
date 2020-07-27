1c76452d8d1c0d6070ce89fe7ae162e6e094e295
Alex Mata <alexmata92@ymail.com>
2020-07-27 16:32:39 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,3 @@
 first_number = 27
+
 second_number = 4
-
-#{first_number} + #{second_number}
-
-`#{first_number} - #{second_number}`
-
-`#{first_number} * #{second_number}`
-
-`#{first_number} / #{second_number}`
